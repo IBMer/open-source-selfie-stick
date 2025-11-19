6f1d29778bd3c233be3fb2f5893f5b4328eb6b23
Vincent WANG <vincent.x.h.wang@hotmail.com>
2025-11-19 08:49:12 +0800

    add initial IOO Xcode project

---
+++ IOO/IOO/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  IOO
+//
+//  Created by Vincent WANG on 2025/11/19.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
